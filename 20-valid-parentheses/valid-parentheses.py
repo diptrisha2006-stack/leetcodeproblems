ae02a76ba215d0c73264e9f2be84e9364f93de97
diptrisha2006-stack <diptrisha2006@gmail.com>
2026-09-18 11:38:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.83%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_b = "({["
+        close_b = ")}]"
+        st = []
+        for i in s:
+            if i in open_b:
+                st.append(i)
+            else:
+                if not st:
+                    return False
+                else:
+                    if i==')' and st[-1] == '(' or i=='}' and st[-1] == '{' or i==']' and st[-1] == '[':
+                        st.pop()
+                    else:
+                        return False
+        return not st
